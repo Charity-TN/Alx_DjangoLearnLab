8cfce7e4f4b9c8a1b03b5f85c77e039f629ccfe8
Charity Thuo <charitynthuo@gmail.com>
2025-03-18 23:20:41 +0300

    Added some view codes.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_book.html', {'books':books})
+
+from django.views.generic import ListView
+
+
+class LibraryBookListView(ListView):
+    model = Book
+    template_name = 'library_books.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        library_id = self.kwargs.get('library_id')
+        return Book.objects.filter(library_id=library_id)
+    
+
