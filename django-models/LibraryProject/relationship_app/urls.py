b305d4cfb3129233e691d832afa95eb26ac4afdb
Charity Thuo <charitynthuo@gmail.com>
2025-03-20 12:39:00 +0300

    Created the file and added some urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from relationship_app import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-path('logout/', views.logout_view, name='logout'),
-path('register/', views.register_view, name='register'),
+    path('login/', LoginView.as_view(template_name= 'login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 
 ]
