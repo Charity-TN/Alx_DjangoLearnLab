b998a7e36a8fc5b84df246efb57959bdc6d26c59
Charity Thuo <charitynthuo@gmail.com>
2025-03-20 12:07:51 +0300

    Added some urls for login,logout and register

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -17,10 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from relationship_app.views import list_books, LibraryDetailView
+from django.contrib.auth import views as auth_views
+from relationship_app.views import register
+from relationship_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('books/', list_books, name='list-books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(),name='library_detail'),
-    
+    path('login/',auth_views.LoginView.as_view(template_name='relationship_app/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'),name='logout'),
+    path('register/',register,name='register'),
+
 ]
