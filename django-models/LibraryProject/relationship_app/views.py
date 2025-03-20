fe299cb1db73a41d7cc9d4a8c7df8e8759fe8ccd
Charity Thuo <charitynthuo@gmail.com>
2025-03-20 12:32:10 +0300

    Added some function-based views for the created url.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -39,3 +39,14 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to the homepage!")
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,logout, authenticate
+
+def login_view(request):
+    return render(request, 'login.html')
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+def register_view(request):
+    return render(request, 'register.html')
