2568806a8860367338621275586aee745db672c9
Charity Thuo <charitynthuo@gmail.com>
2025-03-20 09:36:38 +0300

    Modified the both view codes.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Book
 
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_book.html', {'books':books})
-
-from django.views.generic import ListView
-
+    return render(request, 'relationship_app/list_books.html', {'books':books})
 
 class LibraryBookListView(ListView):
     model = Book
-    template_name = 'library_books.html'
+    template_name = 'relationship_app/list_books.html'
     context_object_name = 'books'
 
     def get_queryset(self):
         library_id = self.kwargs.get('library_id')
-        return Book.objects.filter(library_id=library_id)
+        if library_id:
+            return Book.objects.filter(library_id=library_id)
+        return Book.objects.all()
     
 
