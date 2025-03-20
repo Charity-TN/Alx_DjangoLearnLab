b51c264e1f7b1375586923431f7b52618610ef07
Charity Thuo <charitynthuo@gmail.com>
2025-03-20 09:52:06 +0300

    Modified the urls.

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from relationship_app.views import list_books, LibraryBookListView
+from relationship_app.views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', list_books, name='list-books'),
-    path('library/<int:library_id>/ books/', LibraryBookListView.as_view(),name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(),name='library_detail'),
     
 ]
