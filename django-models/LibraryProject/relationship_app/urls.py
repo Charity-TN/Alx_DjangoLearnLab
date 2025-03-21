b1ce5d3bbe7600dcbecd10fd6bad3bdcfbd973c3
Charity Thuo <charitynthuo@gmail.com>
2025-03-21 14:05:18 +0300

    imported the list_books and LibraryDetailView modules from views.py.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from relationship_app import list_books, LibraryDetailView, views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int>:pk/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register_view, name='register'),
-
+    
 ]
