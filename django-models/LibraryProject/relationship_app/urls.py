87b3f439f7ca1235597987ae61c48a548144f620
Charity Thuo <charitynthuo@gmail.com>
2025-03-21 13:58:43 +0300

    imported the list_books and LibraryDetailView modules.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from relationship_app import views
+from relationship_app import list_books, LibraryDetailView, views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name= 'login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int>:pk/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register_view, name='register'),
 
 ]
