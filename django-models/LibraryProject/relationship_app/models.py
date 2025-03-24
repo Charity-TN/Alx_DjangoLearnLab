c1279263803590fe45ed7d2027c07204550dd62f
Charity Thuo <charitynthuo@gmail.com>
2025-03-24 13:05:15 +0300

    Modified the code.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -19,7 +19,7 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
 from django.contrib.auth.models import User
-from django.db import models
+
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
@@ -29,7 +29,7 @@ class UserProfile(models.Model):
     ]
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
