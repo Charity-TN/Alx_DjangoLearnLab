a146b76dd58b47e28c9c6e11dc298c21cb54ec44
Charity Thuo <charitynthuo@gmail.com>
2025-03-24 13:52:21 +0300

    Added the urls for the permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int>:pk/', LibraryDetailView.as_view(), name='library_detail'),
-    
+    path('add/', views.add_book, name='add_book'),
+    path('edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
+]
 ]
