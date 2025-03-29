397541e6ce286470703b1e98f19c5e5b3e830027
Charity Thuo <charitynthuo@gmail.com>
2025-03-29 19:09:58 +0300

    Modified the code.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -55,27 +55,27 @@ def register_view(request):
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
-from relationship_app.models import UserProfile
+
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.is_authenticated and getattr(user, "userprofile", None) and user.userprofile.role == "Admin"
 
-@user_passes_test(is_admin)
+@user_passes_test(lambda u: is_admin(u))
 def admin_view(request):
     return render(request,'relationship_app/admin_view.html')
 
 def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+    return user.is_authenticated and getattr(user, "userprofile", None) and user.userprofile.role == "Librarian"
 
-@user_passes_test(is_librarian)
-def librarian_view(request):
+@user_passes_test(lambda u: is_librarian(u))
+def admin_view(request):
     return render(request,'relationship_app/librarian_view.html')
 
 def is_member(user):
-    return user.userprofile.role == 'Member'
+    return user.is_authenticated and getattr(user, "userprofile", None) and user.userprofile.role == "Member"
 
-@user_passes_test(is_member)
-def member_view(request):
+@user_passes_test(lambda u: is_member(u))
+def admin_view(request):
     return render(request,'relationship_app/member_view.html')
 
 from django.shortcuts import render, get_object_or_404, redirect
