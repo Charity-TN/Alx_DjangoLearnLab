affe85f349ee69778d8733da88dae88dced3a8e4
Charity Thuo <charitynthuo@gmail.com>
2025-03-29 18:42:30 +0300

    Created the admin_view, librarian_view and member_view views.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -40,6 +40,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Welcome to the homepage!")
 
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login,logout, authenticate
 
@@ -51,29 +52,31 @@ def logout_view(request):
 def register_view(request):
     return render(request, 'register.html')
 
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from relationship_app.models import UserProfile
 
 def is_admin(user):
-    return user.is_authenticated and getattr(user, 'userprofile', None) and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return user.is_authenticated and getattr(user, 'userprofile', None) and user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.is_authenticated and getattr(user, 'userprofile', None) and user.userprofile.role == 'Member'
-    
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')    
+    return render(request,'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request,'relationship_app/librarian_view.html')
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')        
+    return render(request,'relationship_app/member_view.html')
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
