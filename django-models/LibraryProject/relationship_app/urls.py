37b55269b4664f199581d3a65c309dec6428bd86
Charity Thuo <charitynthuo@gmail.com>
2025-03-29 18:58:14 +0300

    Added the admin_view,member_view and librarian_view urls.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from relationship_app.views import list_books, LibraryDetailView
 from .views import add_book, edit_book, delete_book, book_list
-
+from . import views
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int>:pk/', LibraryDetailView.as_view(), name='library_detail'),
@@ -9,5 +9,9 @@ urlpatterns = [
     path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
     path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
     path('', book_list, name='book_list'),
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
+
 ]
-]
+
