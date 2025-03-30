81661495ca6bf9c3ad2989b576064888cab68a97
Charity Thuo <charitynthuo@gmail.com>
2025-03-30 18:58:22 +0300

    Includes permission checks in various views.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
+
+@permission_required('app_name.can_edit', raise_exception=True)
+def edit_view(request, pk):
+    # Add your edit logic here
+    return render(request, 'edit.html')
+
+@permission_required('app_name.can_view', raise_exception=True)
+def view_view(request):
+    # Add your view logic here
+    return render(request, 'view.html')
