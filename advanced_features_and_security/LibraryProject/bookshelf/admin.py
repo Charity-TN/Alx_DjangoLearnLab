ca3ee3108a3ec42c179d6d1868967ed6c67730ef
Charity Thuo <charitynthuo@gmail.com>
2025-03-30 13:36:44 +0300

    Added some code.

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -12,3 +12,19 @@ class BookAdmin(admin.ModelAdmin):
 
 #Enable search by title and author
     list_filter = ('publication_year',)
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
