26082b9670bd306052b321c24db72a26ba34f818
Charity Thuo <charitynthuo@gmail.com>
2025-03-31 10:22:10 +0300

    Added the url pattern for obtain_auth_token.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,13 @@
-from views import BookList, BookViewSet
+from .views import BookList, BookViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('', include(router.urls)),  # This includes all routes registered with the router
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+
 ]
