a990aaf66cb8ad936e2311621506c070e5c4337c
Charity Thuo <charitynthuo@gmail.com>
2025-04-14 00:28:31 +0300

    Modified.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from .models import Post
+from taggit.forms import TagWidget
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
-
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(attrs={'placeholder': 'Add tags separated by commas'}),
+        }
+        
 from django import forms
 from .models import Comment
 
