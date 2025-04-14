9baf5e7dae3a170f5aa3d6e92cf075c3922e8ed6
Charity Thuo <charitynthuo@gmail.com>
2025-04-14 22:06:18 +0300

    Modified.

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -17,6 +17,7 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
@@ -24,3 +25,15 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
+
+class UserFeedAPIView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
