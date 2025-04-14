daded36fa850bbc36375d232bec966a1f641e5a5
Charity Thuo <charitynthuo@gmail.com>
2025-04-14 21:23:01 +0300

    Modified.

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status, generics, permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from .serializers import UserRegistrationSerializer, UserProfileSerializer
+
+class RegistrationAPIView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        # Retrieve the created user (assuming username is unique)
+        user = self.serializer_class.Meta.model.objects.get(username=response.data['username'])
+        token, _ = Token.objects.get_or_create(user=user)
+        data = {
+            "username": user.username,
+            "token": token.key,
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                             context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'username': user.username,
+        })
+
+class UserProfileAPIView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
