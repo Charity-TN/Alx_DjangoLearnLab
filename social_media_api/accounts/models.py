19c605aea83b522d33576a8d2613d3a24af2a6c5
Charity Thuo <charitynthuo@gmail.com>
2025-04-14 22:07:31 +0300

    Modified.

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -10,9 +10,21 @@ class CustomUser(AbstractUser):
     followers = models.ManyToManyField(
         'self', 
         symmetrical=False, 
-        blank=True, 
-        related_name='following'
+        related_name='following', 
+        blank=True
     )
+
+    def follow(self, user):
+        """Follow a user"""
+        self.following.add(user)
+
+    def unfollow(self, user):
+        """Unfollow a user"""
+        self.following.remove(user)
+
+    def is_following(self, user):
+        """Check if following a user"""
+        return self.following.filter(id=user.id).exists()
 
     def __str__(self):
         return self.username
