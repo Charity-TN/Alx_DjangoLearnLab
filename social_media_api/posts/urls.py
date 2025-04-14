a6221e704e92d891ffb29b3e00b4f2294efef956
Charity Thuo <charitynthuo@gmail.com>
2025-04-14 22:05:44 +0300

    Modified.

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,3 +9,18 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+from django.urls import path
+from .views import FollowUserAPIView, UnfollowUserAPIView
+
+urlpatterns = [
+    path('follow/<int:user_id>/', FollowUserAPIView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserAPIView.as_view(), name='unfollow-user'),
+]
+
+from django.urls import path
+from .views import UserFeedAPIView
+
+urlpatterns = [
+    path('feed/', UserFeedAPIView.as_view(), name='user-feed'),
+]
