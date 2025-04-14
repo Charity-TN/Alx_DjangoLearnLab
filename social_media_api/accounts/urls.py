56018358f5aa5b068f10aef970f6ad785f57341f
Charity Thuo <charitynthuo@gmail.com>
2025-04-14 22:14:02 +0300

    Modified.

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,3 +6,11 @@ urlpatterns = [
     path('login/', CustomObtainAuthToken.as_view(), name='login'),
     path('profile/', UserProfileAPIView.as_view(), name='profile'),
 ]
+
+from django.urls import path
+from .views import FollowUserAPIView, UnfollowUserAPIView
+
+urlpatterns = [
+    path('follow/<int:user_id>/', FollowUserAPIView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserAPIView.as_view(), name='unfollow-user'),
+]
