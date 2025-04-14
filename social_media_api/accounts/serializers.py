f4ea6ce7bb1f15456714e2396b998d67c7e10748
Charity Thuo <charitynthuo@gmail.com>
2025-04-14 21:22:03 +0300

    Modified.

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password', 'email', 'bio', 'profile_picture')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture')
+        )
+        return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
